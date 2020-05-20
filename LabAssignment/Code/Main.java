4bcd278fcf79e490397b49cba97bd55fb0fe4da2
Fatih Sevban Uyanık <fatihsevban15@gmail.com>
2020-05-20 15:27:12 +0300

    initial commit

---
+++ LabAssignment/Code/Main.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<Integer> list3 = new ArrayList<>();
+
+        list1.add(100); list1.add(500); list1.add(900);
+        list2.add(800); list2.add(600); list2.add(300);
+        list3.add(400); list3.add(700); list3.add(200);
+
+        ArrayOrganizer arrayOrganizer = new ArrayOrganizer();
+        ArrayList<Integer> result = arrayOrganizer.concatAndSort(
+                list1, list2, list3);
+
+        System.out.println("Result => " + result);
+        System.out.println("-----------------------------");
+
+        Die die = new Die();
+        Player player1 = new Player("Tom");
+        Player player2 = new Player("John");
+        Player player3 = new Player("James");
+        Player player4 = new Player("Henry");
+
+        for (int i = 0; i < 5; i++) {
+            player1.addScore( die.rollDie() );
+            player2.addScore( die.rollDie() );
+            player3.addScore( die.rollDie() );
+            player4.addScore( die.rollDie() );
+        }
+
+        System.out.println(player1);
+        System.out.println(player2);
+        System.out.println(player3);
+        System.out.println(player4);
+    }
+}
